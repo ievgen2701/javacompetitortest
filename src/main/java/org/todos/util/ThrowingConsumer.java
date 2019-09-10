e5389a2a6d1b9483ae10162d73048a92daef37b1
ievgen2701 <ievgen.anikushyn@gamil.com>
2019-09-10 14:43:22 +0300

    added utility classes for wrapping checked exceptions

---
+++ src/main/java/org/todos/util/ThrowingConsumer.java
@@ -0,0 +1,24 @@
+package org.todos.util;
+
+import java.util.function.Consumer;
+
+/**
+ * Consumer is needed to wrap checked exceptions in consumer lambdas.
+ *
+ * @param <T> - acceptable object
+ * @param <E> - thrown exception
+ */
+@FunctionalInterface
+public interface ThrowingConsumer<T, E extends Throwable> {
+    void accept(T t) throws E;
+
+    static <T, E extends Throwable> Consumer<T> unchecked(final ThrowingConsumer<T, E> consumer) {
+        return (t) -> {
+            try {
+                consumer.accept(t);
+            } catch (final Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
---
+++ src/main/java/org/todos/util/ThrowingFunction.java
@@ -0,0 +1,25 @@
+package org.todos.util;
+
+import java.util.function.Function;
+
+/**
+ * Function is needed to wrap checked exceptions in map lambdas.
+ *
+ * @param <T> - input type
+ * @param <R> - returned type
+ * @param <E> - thrown exception
+ */
+@FunctionalInterface
+public interface ThrowingFunction<T, R, E extends Throwable> {
+    R apply(T t) throws E;
+
+    static <T, R, E extends Throwable> Function<T, R> unchecked(final ThrowingFunction<T, R, E> f) {
+        return t -> {
+            try {
+                return f.apply(t);
+            } catch (final Throwable e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
