bc9caa2a27f4e59164efd8a0066773927495433f
ievgen2701 <ievgen.anikushyn@gamil.com>
2019-09-10 14:00:24 +0300

    added main class

---
+++ src/main/java/org/todos/JavaCompetitorApplication.java
@@ -0,0 +1,13 @@
+package org.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCompetitorApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(JavaCompetitorApplication.class, args);
+	}
+
+}
