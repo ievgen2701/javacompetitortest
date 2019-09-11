1ae0c838c4bb4c3d90b2367917f591a2014c3713
ievgen2701 <ievgen.anikushyn@gamil.com>
2019-09-11 15:09:37 +0300

    added holder for java class information

---
+++ src/main/java/org/javacompet/model/JavaClassInfo.java
@@ -0,0 +1,11 @@
+package org.javacompet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public final class JavaClassInfo {
+    private String sourceCode;
+    private String className;
+}
