0a991654ea88ee4c732d0a4ab3e3627534e836e8
ievgen2701 <ievgen.anikushyn@gamil.com>
2019-09-11 16:57:45 +0300

    added view and input repository

---
+++ src/main/java/org/javacompet/repository/TestRepository.java
@@ -0,0 +1,33 @@
+package org.javacompet.repository;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+@Repository
+public class TestRepository {
+
+    private final Resource[] resources;
+
+    @Autowired
+    public TestRepository(final ResourceLoader resourceLoader) throws IOException {
+        resources = new PathMatchingResourcePatternResolver(resourceLoader)
+                .getResources("classpath*:/**/inputs/*.txt");
+    }
+
+    // todo: add more inputs for tasks and indexing to /inputs folder in the resource folder
+    public String inputForTask(final String taskName) throws IOException {
+        final Resource resource = Arrays.stream(resources)
+                .filter(res -> taskName.equals(res.getFilename()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Can't find inputs for the given task: " + taskName));
+        return FileUtils.readFileToString(resource.getFile(), StandardCharsets.UTF_8);
+    }
+}
